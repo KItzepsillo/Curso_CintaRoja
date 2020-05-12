7f07ff291cd3418a683e2a4b11891b5ce2547283
Kevin <kevin.itzep94@gmail.com>
2020-05-12 17:38:52 -0500

    agregue html y css

---
+++ 8_React/teoria.js
@@ -0,0 +1,17 @@
+/*
+React --> Front end
+Dom -> Document object model
+Real Dom
+Virtual Dom
+jsx -> javascript con esteroide
+
+Crear un archivo de React por proyecto
+    -npx create-react-app my-app
+    -cd my-app
+    -npm start
+
+    -reaf  -> crea un componente
+
+Generar en npm build
+    -npm run build
+*/
